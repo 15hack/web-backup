5a75eb8f229c498f9afda9ac79548b42e6e6c879
15hack <15hack@riseup.net>
2020-11-19 10:33:06 +0100

    errata en las estadisticas

--- core/sitedb.py
+++ core/sitedb.py
@@ -144,21 +144,21 @@ class SiteDBLite(DBLite):
             counts={}
         )
         tables = tuple('''
-            sites
-            wp_posts
-            wp_media
-            wp_comments
-            wp_tags
-            phpbb_topics
-            phpbb_posts
-            phpbb_media
-            wk_pages
-            wk_media
+            posts
+            media
+            comments
+            tags
+            pages
+            topics
         '''.strip().split())
-        for t in tables:
-            if t not in self.tables:
-                continue
-            if site is not None and "site" not in self.tables[t]:
+        if site is None:
+            r.counts['sites']={
+                '_total_':self.one("select count(*) from sites")
+            }
+            for tp, c in self.select("select type, count(*) c from sites group by type order by count(*) desc"):
+                r.counts['sites'][tp]=c
+        for t in sorted(self.tables):
+            if t not in tables and t.split("_", 1)[-1] not in tables:
                 continue
             r.counts[t]=self.one("select count(*) from "+t+where)
         return r
@@ -166,7 +166,15 @@ class SiteDBLite(DBLite):
     def print_info(self, file, table_link=True):
         md=MDWriter(file)
         r = self.get_info()
-        md.write("Datos desde el {ini} al {fin}:".format(**dict(r)), end="\n\n")
+        md.write(dedent('''
+            Se han escaneado {t_sites} `sites`, con datos desde el {ini} al {fin},
+            repartidos en:
+        ''').format(t_sites=r.counts['sites']['_total_'], **dict(r)), end="\n\n")
+        del r.counts['sites']['_total_']
+        for t, c in r.counts['sites'].items():
+            s = "" if c == 1 else "s"
+            md.write("* {rows} sitio{s} `{type}`".format(type=t, rows=c, s=s))
+        del r.counts['sites']
         for t, c in r.counts.items():
             md.write("* {rows} registros en `{table}`".format(table=t, rows=c))
         if "url" in self.tables["sites"]:
@@ -197,7 +205,9 @@ class SiteDBLite(DBLite):
                 row["fin"] = info.fin
                 row["site"] = url
                 row["admin"] = "https://{}/wp-admin/".format(url)
-                row["ult_comment"] = self.one("select IFNULL(substr(date, 1, 10), '') from wp_comments")
+                row["ult_comment"] = self.one("select IFNULL(substr(max(date), 1, 10), '') from wp_comments where site="+str(id))
+                if row["ult_comment"] is None:
+                    row["ult_comment"] = ""
                 if table_link:
                     md_row = build_tr('''
                         [{site}](https://{site})
